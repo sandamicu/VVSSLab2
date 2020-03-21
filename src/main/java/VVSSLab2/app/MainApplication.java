17a884e7c4db689e07446bc91b2459b212d3af86
Sanda Micu <sanda.micu@p3-holding.com>
2020-03-21 22:17:21 +0200

    Tests for addStudent

---
+++ src/main/java/VVSSLab2/app/MainApplication.java
@@ -0,0 +1,36 @@
+package VVSSLab2.app;
+
+
+import VVSSLab2.repository.NotaXMLRepo;
+import VVSSLab2.repository.StudentXMLRepo;
+import VVSSLab2.repository.TemaXMLRepo;
+import VVSSLab2.service.Service;
+import VVSSLab2.validation.NotaValidator;
+import VVSSLab2.validation.StudentValidator;
+import VVSSLab2.validation.TemaValidator;
+import VVSSLab2.view.UI;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+       //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
+        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
+        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
+        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        UI ui = new UI(service);
+        ui.run();
+    }
+
+}
--- src/test/java/VVSSLab2/AppTest.java
+++ src/test/java/VVSSLab2/AppTest.java
@@ -1,20 +1,64 @@
 package VVSSLab2;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import VVSSLab2.domain.Student;
+import VVSSLab2.repository.NotaXMLRepo;
+import VVSSLab2.repository.StudentXMLRepo;
+import VVSSLab2.repository.TemaXMLRepo;
+import VVSSLab2.service.Service;
+import VVSSLab2.validation.NotaValidator;
+import VVSSLab2.validation.StudentValidator;
+import VVSSLab2.validation.TemaValidator;
+import VVSSLab2.validation.ValidationException;
+import junit.framework.TestCase;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private StudentValidator studentValidator;
+    private TemaValidator temaValidator;
+    String filenameStudent = "fisiere/Studenti.xml";
+    String filenameTema = "fisiere/Teme.xml";
+    String filenameNota = "fisiere/Note.xml";
+    private StudentXMLRepo studentXMLRepository;
+    private TemaXMLRepo temaXMLRepository;
+    private NotaValidator notaValidator;
+    private NotaXMLRepo notaXMLRepository;
+    private Service service;
+
+    @BeforeEach
+    public void setup() {
+        studentValidator = new StudentValidator();
+        temaValidator = new TemaValidator();
+        studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        temaXMLRepository = new TemaXMLRepo(filenameTema);
+        notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        notaXMLRepository = new NotaXMLRepo(filenameNota);
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testStudent() {
+        setup();
+        Student student = new Student("1234", "Sanda", 934, "sanda@gmail.com");
+        assertEquals(service.addStudent(student), student);
+    }
+
+    @Test
+    public void testStudentFail() {
+        setup();
+        Student student = new Student("1234", "Sanda", -934, "sanda@gmail.com");
+        Exception exception = assertThrows(ValidationException.class, () -> service.addStudent(student));
+
+        String expectedMessage = "Grupa incorecta!";
+        String actualMessage = exception.getMessage();
+
+        TestCase.assertTrue(actualMessage.contains(expectedMessage));
     }
 }
