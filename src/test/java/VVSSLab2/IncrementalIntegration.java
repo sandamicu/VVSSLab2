9b0ab8d2ba0028d63edd3fd83542688f447f6d21
Diana-Georgiana Mocanu <dianagmocanu@gmail.com>
2020-05-16 15:50:07 +0300

    take home incremental

---
+++ src/test/java/VVSSLab2/IncrementalIntegration.java
@@ -0,0 +1,84 @@
+package VVSSLab2;
+
+import VVSSLab2.domain.Nota;
+import VVSSLab2.domain.Student;
+import VVSSLab2.domain.Tema;
+import VVSSLab2.repository.NotaXMLRepo;
+import VVSSLab2.repository.StudentXMLRepo;
+import VVSSLab2.repository.TemaXMLRepo;
+import VVSSLab2.service.Service;
+import VVSSLab2.validation.NotaValidator;
+import VVSSLab2.validation.StudentValidator;
+import VVSSLab2.validation.TemaValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+
+public class IncrementalIntegration {
+    private StudentValidator studentValidator;
+    private TemaValidator temaValidator;
+    String filenameStudent = "fisiere/Studenti.xml";
+    String filenameTema = "fisiere/Teme.xml";
+    String filenameNota = "fisiere/Note.xml";
+    private StudentXMLRepo studentXMLRepository;
+    private TemaXMLRepo temaXMLRepository;
+    private NotaValidator notaValidator;
+    private NotaXMLRepo notaXMLRepository;
+    private Service service;
+
+
+    private Integer idStudentForGrade = 123;
+    private String idAssignmentForGrade = "123";
+
+    @BeforeEach
+    public void setup() {
+        studentValidator = new StudentValidator();
+        temaValidator = new TemaValidator();
+        studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        temaXMLRepository = new TemaXMLRepo(filenameTema);
+        notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        notaXMLRepository = new NotaXMLRepo(filenameNota);
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        // add sample data for addGrade
+        // the problem was here, I named the student "name1"
+        Student returnCode = new Student(idStudentForGrade, "Pop Ana-Maria", 0, "ana@gmail.com");
+        assertEquals(service.addStudent(returnCode), returnCode);
+        Tema response = new Tema(idAssignmentForGrade, "description", 5 ,4);
+        assertEquals(service.addTema(response), response);
+    }
+
+    // Test for student success: TestCase #7
+    @Test
+    public void testStudent_success() {
+        Student student = new Student(idStudentForGrade, "Pop Ana-Maria", 0, "ana@gmail.com");
+        assertEquals(service.addStudent(student), student);
+    }
+
+    // Test for assignment success
+    @Test
+    public void testAssignmentStudent_success() {
+        testStudent_success();
+        Tema tema = new Tema(idAssignmentForGrade, "description", 5 ,4);
+        Tema response = service.addTema(tema);
+
+        assertEquals(tema, response);
+        System.out.println();
+    }
+
+    // Test for assignment success
+    @Test
+    public void testNotaAssignmentStudent_success() {
+        testAssignmentStudent_success();
+        Nota nota = new Nota("1", idStudentForGrade.toString(), idAssignmentForGrade, 9.4 , LocalDate.parse("2020-12-03"));
+        double response = service.addNota(nota, "feedback");
+
+        assertEquals (9, (int)response);
+        System.out.println();
+    }
+
+
+}
